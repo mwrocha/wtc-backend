89da702b3033cc7e530f10b1fb7c75f119747a5c
Mayke <mwrrestudos@gmail.com>
2026-05-02 17:00:36 -0300

    refactor: formatação e padronização geral do código
    - Reformatação de todos os arquivos via IntelliJ (Ctrl+Alt+L)
    - Organização de imports
    - Padronização de indentação e estilo

--- src/main/java/br/com/wtc/web/dto/TaskRequest.java
+++ src/main/java/br/com/wtc/web/dto/TaskRequest.java
@@ -11,22 +11,75 @@ public class TaskRequest {
     private String messageRef;
     private String dueDate;
 
-    public String getTitle()           { return title; }
-    public void setTitle(String t)     { this.title = t; }
-    public String getDescription()     { return description; }
-    public void setDescription(String d){ this.description = d; }
-    public String getClientId()        { return clientId; }
-    public void setClientId(String c)  { this.clientId = c; }
-    public String getClientName()      { return clientName; }
-    public void setClientName(String n){ this.clientName = n; }
-    public String getCategory()        { return category; }
-    public void setCategory(String c)  { this.category = c; }
-    public String getPriority()        { return priority; }
-    public void setPriority(String p)  { this.priority = p; }
-    public String getStatus()          { return status; }
-    public void setStatus(String s)    { this.status = s; }
-    public String getMessageRef()      { return messageRef; }
-    public void setMessageRef(String m){ this.messageRef = m; }
-    public String getDueDate()         { return dueDate; }
-    public void setDueDate(String d)   { this.dueDate = d; }
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String t) {
+        this.title = t;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String d) {
+        this.description = d;
+    }
+
+    public String getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(String c) {
+        this.clientId = c;
+    }
+
+    public String getClientName() {
+        return clientName;
+    }
+
+    public void setClientName(String n) {
+        this.clientName = n;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String c) {
+        this.category = c;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String p) {
+        this.priority = p;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String s) {
+        this.status = s;
+    }
+
+    public String getMessageRef() {
+        return messageRef;
+    }
+
+    public void setMessageRef(String m) {
+        this.messageRef = m;
+    }
+
+    public String getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(String d) {
+        this.dueDate = d;
+    }
 }
