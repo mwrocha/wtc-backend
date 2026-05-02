788cfe0f9c8847705e14dd0740e17a4759ec1e4f
Mayke <mwrrestudos@gmail.com>
2026-05-02 17:01:12 -0300

    refactor: formatação e padronização geral do código
    - Reformatação de todos os arquivos via IntelliJ (Ctrl+Alt+L)
    - Organização de imports
    - Padronização de indentação e estilo

--- src/main/java/br/com/wtc/config/MongoConfig.java
+++ src/main/java/br/com/wtc/config/MongoConfig.java
@@ -8,7 +8,7 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 @EnableMongoAuditing
 @EnableMongoRepositories(basePackages = "br.com.wtc.domain.repository")
 public class MongoConfig {
-    // O Spring Boot autoconfigura o MongoDB pela URI do application.yml
+    // O Spring Boot configura o MongoDB pela URI do application.yml
     // Esta classe ativa auditoria (@CreatedDate, @LastModifiedDate)
     // e garante que os repositories são escaneados no pacote correto
 }
