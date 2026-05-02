72c365701c5d71baf063b9afe310790a0feedf18
Mayke <mwrrestudos@gmail.com>
2026-05-02 17:01:41 -0300

    refactor: formatação e padronização geral do código
    - Reformatação de todos os arquivos via IntelliJ (Ctrl+Alt+L)
    - Organização de imports
    - Padronização de indentação e estilo

--- src/main/java/br/com/wtc/web/dto/LoginResponse.java
+++ src/main/java/br/com/wtc/web/dto/LoginResponse.java
@@ -1,9 +1,6 @@
 package br.com.wtc.web.dto;
 
-public record LoginResponse(
-        String token,
-        String id,      // MongoDB ObjectId do usuário
-        String email,
-        String name,
-        String role     // "OPERATOR" ou "CLIENT"
-) {}
+public record LoginResponse(String token, String id,      // MongoDB ObjectId do usuário
+                            String email, String name, String role     // "OPERATOR" ou "CLIENT"
+) {
+}
