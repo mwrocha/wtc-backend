8de0796c92a022db1d69d5e9bfb9d868ef179f58
Mayke <mwrrestudos@gmail.com>
2026-05-02 17:00:40 -0300

    refactor: formatação e padronização geral do código
    - Reformatação de todos os arquivos via IntelliJ (Ctrl+Alt+L)
    - Organização de imports
    - Padronização de indentação e estilo

--- src/main/java/br/com/wtc/domain/repository/TaskRepository.java
+++ src/main/java/br/com/wtc/domain/repository/TaskRepository.java
@@ -2,10 +2,13 @@ package br.com.wtc.domain.repository;
 
 import br.com.wtc.domain.model.Task;
 import org.springframework.data.mongodb.repository.MongoRepository;
+
 import java.util.List;
 
 public interface TaskRepository extends MongoRepository<Task, String> {
     List<Task> findByOperatorIdOrderByCreatedAtDesc(String operatorId);
+
     List<Task> findByClientIdOrderByCreatedAtDesc(String clientId);
+
     List<Task> findByOperatorIdAndStatusOrderByCreatedAtDesc(String operatorId, String status);
 }
