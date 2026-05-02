2efb29d3285c01277c660762844e5b20854bd9d5
Mayke <mwrrestudos@gmail.com>
2026-05-02 17:01:50 -0300

    refactor: formatação e padronização geral do código
    - Reformatação de todos os arquivos via IntelliJ (Ctrl+Alt+L)
    - Organização de imports
    - Padronização de indentação e estilo

--- src/main/java/br/com/wtc/web/dto/LoginRequest.java
+++ src/main/java/br/com/wtc/web/dto/LoginRequest.java
@@ -5,10 +5,7 @@ import jakarta.validation.constraints.NotBlank;
 
 public record LoginRequest(
 
-        @NotBlank(message = "E-mail é obrigatório")
-        @Email(message = "E-mail inválido")
-        String email,
+        @NotBlank(message = "E-mail é obrigatório") @Email(message = "E-mail inválido") String email,
 
-        @NotBlank(message = "Senha é obrigatória")
-        String password
-) {}
+        @NotBlank(message = "Senha é obrigatória") String password) {
+}
